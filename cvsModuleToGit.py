b62589e3dfcb9090faac2aad912e120ad48dcd70
Hugo Slepicka <slepicka@slac.stanford.edu>
2017-02-10 16:30:24 -0800

    Adding Kernel Module option to cvsModuleToGit.py

--- cvsModuleToGit.py
+++ cvsModuleToGit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-'''This script import an LCLS EPICS module from CVS to a git repo.'''
+'''This script import an LCLS module from CVS to a git repo.'''
 
 import argparse
 import os.path
@@ -7,21 +7,34 @@ import shutil
 import tempfile
 from git_utils import *
 
-defaultModulesDir = "/afs/slac/g/cd/swe/git/repos/package/epics/modules/from-cvs"
+gitDefaultDirFormat = "/afs/slac/g/cd/swe/git/repos/package/{}/from-cvs"
 
-def importModule( module ):
-    CVSpackageLocation = os.path.join( os.environ['CVSROOT'], 'epics/site/src', module )
+# Dictionary to store the cvs path and git path for each kind of import
+# To add a new type add the entry in here with the proper paths and add
+# the proper definition of moduleType for the importModule function.
+moduleTypePaths = {
+    'epics_module': {'cvs': 'epics/site/src', 'git': 'epics/modules'},
+    'kernel_module': {'cvs': 'linuxKernel_Modules', 'git': 'linux/drivers/kernel'}
+}
+
+
+def importModule( module, module_type ):
+    tp = moduleTypePaths[module_type]
+
+    CVSpackageLocation = os.path.join( os.environ['CVSROOT'], tp['cvs'], module )
     print "Importing CVS module %s from %s" % ( module, CVSpackageLocation )
  
     # Import the CVS history using a tmp folder
     tpath = tempfile.mkdtemp()
-    importHistoryFromCVS(tpath, None, CVSpackageLocation, modulesDir=defaultModulesDir, module=module )
+    modulesDir = gitDefaultDirFormat.format(tp['git'])
+    importHistoryFromCVS(tpath, None, CVSpackageLocation, modulesDir=modulesDir, module=module )
     shutil.rmtree(tpath)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser( description='''This script creates a git repo for each specified CVS module....
 ''')
     parser.add_argument( '-m', '--module', action='append', required=True, help='CVS module name to import.' )
+    parser.add_argument( '-k', '--kernel', action='store_true', default=False, required=False, help='Indicates if this is a Kernel Module.' )
 
     args = parser.parse_args( )
 
@@ -30,8 +43,12 @@ if __name__ == '__main__':
         if os.path.exists( '/afs/slac/g/lcls/tools' ):
             os.environ['TOOLS'] = '/afs/slac/g/lcls/tools'
 
+    moduleType = 'epics_module'
+    if args.kernel:
+        moduleType = 'kernel_module'
+
     for m in args.module:
-        importModule( m )
+        importModule( m , moduleType )
 
     print "Done."
 
