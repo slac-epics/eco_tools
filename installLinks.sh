44425635eae4b46b3dd894ac9463578d1eaaf30b
Bruce Hill <bhill@slac.stanford.edu>
2023-02-08 10:47:55 -0800

    Reworked installLinks to setup eco env before running python

---
+++ installLinks.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+this_script=`readlink -f ${BASH_SOURCE[0]}`
+eco_tools_dir=`readlink -f $(dirname $this_script)`
+source $eco_tools_dir/setup_eco_env.sh
+
+$eco_tools_dir/installLinks.py $*
